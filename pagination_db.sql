781e0dae0c40bbe75777619a2fdb346ae09451df
Rendy Ananda Aceda <53991487+rendy-ananda@users.noreply.github.com>
2019-08-22 02:37:58 +0700

    Update pagination_db.sql

--- pagination_db.sql
+++ pagination_db.sql
@@ -1,22 +1,12 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.3.9-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             9.5.0.5196
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
--- Dumping database structure for pagination_db
 CREATE DATABASE IF NOT EXISTS `pagination_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `pagination_db`;
 
--- Dumping structure for table pagination_db.items
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item` varchar(50) DEFAULT NULL,
@@ -24,7 +14,6 @@ CREATE TABLE IF NOT EXISTS `items` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table pagination_db.items: ~6 rows (approximately)
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
 INSERT INTO `items` (`id`, `item`, `price`) VALUES
 	(1, 'processor', 722000),
@@ -34,7 +23,6 @@ INSERT INTO `items` (`id`, `item`, `price`) VALUES
 	(5, 'optical_disk', 179000),
 	(6, 'cooler', 95000);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
